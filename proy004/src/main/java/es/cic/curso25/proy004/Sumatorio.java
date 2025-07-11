b4dd865bcc18c7c2cad3e02731ef75ad000425ae
noname <mail@example.mail>
2025-07-11 12:24:59 +0200

    cosas cosas ..... cosas

--- proy004/src/main/java/es/cic/curso25/proy004/Sumatorio.java
+++ proy004/src/main/java/es/cic/curso25/proy004/Sumatorio.java
@@ -5,6 +5,12 @@ public class Sumatorio {
     
     public void lanzar(){
         int [] a = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        for(int i=0;i<a.length;i++){
+            a[i]=i+1;
+        }
+
+        
     }
 
 }
---
+++ proy004/src/test/java/es/cic/curso25/proy004/SumatorioTest.java
@@ -0,0 +1,11 @@
+package es.cic.curso25.proy004;
+
+import org.junit.jupiter.api.Test;
+
+public class SumatorioTest {
+    
+    
+    @Test
+    void testLanzar() {
+    }
+}
